feea8c3697b07b04e711a4fee2723686c9cc22d9
Jassperino <kszumowski02@gmail.com>
2025-08-18 15:51:50 +0200

    Aktualizacja całego projektu#4

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -16,6 +16,7 @@ public partial class MainWindowViewModel : ViewModelBase
     private string _resultLabel = "Dane";
 
     public ObservableCollection<User> Users { get; } = [];
+    public ObservableCollection<Cryptocurrency> Cryptocurrencies { get; } = [];
 
     public async Task UpdateUsersAsync()
     {
@@ -28,6 +29,14 @@ public partial class MainWindowViewModel : ViewModelBase
         
         Users.Clear();
         
+        var cryptocurrencyData = await dbContext.Cryptocurrencies
+            .AsNoTracking()
+            .Include(c => c.User)
+            .OrderBy(c => c.User.Username)
+            .ToListAsync();
+
+        Cryptocurrencies.Clear();
+
         var sb = new StringBuilder();
         
         sb.AppendLine($"{"Id",-10} {"Username",-20} {"Password"}");
@@ -37,6 +46,16 @@ public partial class MainWindowViewModel : ViewModelBase
             Users.Add(user);
             sb.AppendLine($"{user.Id,-10} {user.Username,-20} {user.Password}");
         }
+        
+        sb.AppendLine();
+        sb.AppendLine($"{"User",-20} {"Bitcoin",-10} {"Ethereum",-10} {"Tether",-10} {"ZCash"}");
+
+        foreach (var crypto in cryptocurrencyData)
+        {
+            Cryptocurrencies.Add(crypto);
+            sb.AppendLine($"{crypto.User.Username,-20} {crypto.Bitcoin,-10} {crypto.Ethereum,-10} {crypto.Tether,-10} {crypto.ZCash}");
+        }
+
 
         ResultLabel = sb.ToString();
     }
--- obj/Debug/net9.0/JIPP_Projekt_Sem4.AssemblyInfo.cs
+++ obj/Debug/net9.0/JIPP_Projekt_Sem4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JIPP_Projekt_Sem4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71207a72b9310e62cdcc2225995e805a49bcf4ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca160ccddbd7e30bfa863a7a4728bc2dc195e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("JIPP_Projekt_Sem4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JIPP_Projekt_Sem4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
